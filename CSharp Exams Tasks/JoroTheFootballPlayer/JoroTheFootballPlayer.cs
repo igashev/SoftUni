9a95ef5c259e5ebe4a16323b6027017e47692ae0
gasho <ivaylo.gashev@gmail.com>
2015-02-10 14:59:59 +0200

    Added JoroTheFootballPlayer

---
+++ CSharp Exams Tasks/JoroTheFootballPlayer/JoroTheFootballPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class JoroTheFootballPlayer
+{
+    public static void Main()
+    {
+        string year = Console.ReadLine();
+        double numberOfHolidays = double.Parse(Console.ReadLine());
+        double numberOfWeekendsInHometown = double.Parse(Console.ReadLine());
+
+        double playsInNormalWeekends = (2.0 / 3.0) * (52.0 - numberOfWeekendsInHometown);
+        double playsInHolidays = (1.0 / 2.0) * numberOfHolidays;
+        double totalPlays = 0;
+        int playsIfYearIsLeap = 3;
+
+        if (year == "t")
+        {
+            totalPlays = playsIfYearIsLeap + playsInHolidays + playsInNormalWeekends + numberOfWeekendsInHometown;
+        }
+        else
+        {
+            totalPlays = playsInHolidays + playsInNormalWeekends + numberOfWeekendsInHometown;
+        }
+
+        Console.WriteLine((int)totalPlays);
+    }
+}
+
