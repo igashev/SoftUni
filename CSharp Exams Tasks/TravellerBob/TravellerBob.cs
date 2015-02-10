62dc50ce34df43fb7f4000ece43309287c59e641
gasho <ivaylo.gashev@gmail.com>
2015-02-10 15:11:04 +0200

    Added TravellerBob

---
+++ CSharp Exams Tasks/TravellerBob/TravellerBob.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class TravellerBob
+{
+    static void Main()
+    {
+        const int ContractTravelsPerMonth = 4 * 3;
+        const int FamilyTravelPerMonth = 2 * 2;
+
+        string kindOfYear = Console.ReadLine();
+        int numberOfContractMonths = int.Parse(Console.ReadLine());
+        int numberOfFamilyMonths = int.Parse(Console.ReadLine());
+
+        int monthsLeft = 12 - (numberOfContractMonths + numberOfFamilyMonths);
+        int totalContractTravels = numberOfContractMonths * ContractTravelsPerMonth;
+        int totalFamilyTravels = numberOfFamilyMonths * FamilyTravelPerMonth;
+        double totalNormalTravels = monthsLeft * ContractTravelsPerMonth * 0.6;
+        double totalTravels = totalContractTravels + totalFamilyTravels + totalNormalTravels;
+
+        if (kindOfYear == "leap")
+        {
+            totalTravels += totalTravels * 0.05;
+        }
+
+        Console.WriteLine((int)totalTravels);
+    }
+}
