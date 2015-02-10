87cab8514b2ec6df95809284ec22ba638efe3da5
gasho <ivaylo.gashev@gmail.com>
2015-02-10 14:48:41 +0200

    Added OddEvenSum

---
+++ CSharp Exams Tasks/OddEvenSum/OddEvenSum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class OddEvenSum
+{
+    public static void Main()
+    {
+        int countOfNumbers = int.Parse(Console.ReadLine());
+        int[] numbers = new int[countOfNumbers * 2];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        int sumOfEvenNumbers = 0;
+        int sumOfOddNumbers = 0;
+        bool isOdd = true;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (isOdd)
+            {
+                sumOfOddNumbers += numbers[i];
+            }
+            else
+            {
+                sumOfEvenNumbers += numbers[i];
+            }
+
+            isOdd = !isOdd;
+        }
+
+        if (sumOfEvenNumbers == sumOfOddNumbers)
+        {
+            Console.WriteLine("Yes, sum=" + sumOfEvenNumbers);
+        }
+        else
+        {
+            Console.WriteLine("No, diff=" + Math.Abs(sumOfEvenNumbers - sumOfOddNumbers));
+        }
+    }
+}
