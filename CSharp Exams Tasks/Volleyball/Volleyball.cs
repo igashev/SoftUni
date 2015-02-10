b3b2ac0ff2da901565be1216cc28fac31c73aa55
gasho <ivaylo.gashev@gmail.com>
2015-02-10 14:42:03 +0200

    Added Volleyball

---
+++ CSharp Exams Tasks/Volleyball/Volleyball.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Volleyball
+{
+    public static void Main()
+    {
+        string typeOfYear = Console.ReadLine();
+        double numberOfHolidays = double.Parse(Console.ReadLine());
+        double numberOfWeekendsInHometown = double.Parse(Console.ReadLine());
+
+        double playsInHometown = numberOfWeekendsInHometown;
+        double playsInNormalWeekends = (48.0 - numberOfWeekendsInHometown) * (3.0 / 4.0);
+        double playsInHolidays = numberOfHolidays * (2.0 / 3.0);
+        double totalPlays = playsInHolidays + playsInHometown + playsInNormalWeekends;
+        
+        if (typeOfYear == "leap")
+        {
+            totalPlays *= 1.15;
+        }
+
+        Console.WriteLine((int)totalPlays);
+    }
+}
