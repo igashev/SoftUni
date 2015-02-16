1d654ea432052a2be6bc39792405e5e24ca1c705
gasho <ivaylo.gashev@gmail.com>
2015-02-17 00:15:31 +0200

    Added WorkHours

---
+++ CSharp Exams Tasks/WorkHours/WorkHours.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class WorkHours
+{
+    public static void Main()
+    {
+        int requeredHours = int.Parse(Console.ReadLine());
+        int daysAvailable = int.Parse(Console.ReadLine());
+        int productivityPercentage = int.Parse(Console.ReadLine());
+
+        int bikingPercentage = 10;
+        double hoursAvailable = (daysAvailable - (daysAvailable * (bikingPercentage / 100.0))) * 12;
+        double efficientWorkHours = Math.Floor(hoursAvailable * (productivityPercentage / 100.0));
+        int result = (int)efficientWorkHours - requeredHours;
+
+        if (result < 0)
+        {
+            Console.WriteLine("{0}\n{1}", "No", result);
+        }
+        else
+        {
+            Console.WriteLine("{0}\n{1}", "Yes", result);
+        }
+    }
+}
