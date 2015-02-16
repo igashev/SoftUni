88f00c982b5095d568588434a7e7ce2be5866489
gasho <ivaylo.gashev@gmail.com>
2015-02-17 00:18:03 +0200

    Added SumOfElements

---
+++ CSharp Exams Tasks/SumOfElements/SumOfElements.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class SumOfElements
+{
+    public static void Main()
+    {
+        string[] inputNumbers = Console.ReadLine().Split(' ');
+        int[] numbers = Array.ConvertAll(inputNumbers, int.Parse);
+
+        Array.Sort(numbers);
+
+        int highestNumber = numbers[numbers.Length - 1];
+        int sumOfOtherNumbers = 0;
+
+        for (int i = 0; i < numbers.Length - 1; i++)
+        {
+            sumOfOtherNumbers += numbers[i];
+        }
+
+        if (highestNumber == sumOfOtherNumbers)
+        {
+            Console.WriteLine("Yes, sum=" + highestNumber);
+        }
+        else
+        {
+            Console.WriteLine("No, diff=" + Math.Abs(highestNumber - sumOfOtherNumbers));
+        }
+    }
+}
