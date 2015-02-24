8d34ddd29d1d86bdf1ca3c4e518f6e6bd5f86529
gasho <ivaylo.gashev@gmail.com>
2015-02-24 18:17:48 +0200

    Added Pairs

---
+++ CSharp Exams Tasks/Pairs/Pairs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class Pairs
+{
+    private static void Main()
+    {
+        string[] userInputNumbers = Console.ReadLine().Split(' ');
+        int[] userDigits = Array.ConvertAll(userInputNumbers, int.Parse);
+        int sum = userDigits[0] + userDigits[1];
+        int counter = 0;
+        List<int> diffCounter = new List<int>();
+
+        for (int i = 0; i < userDigits.Length - 1; i += 2)
+        {
+            if ((userDigits[i] + userDigits[i + 1]) != sum)
+            {
+                if (i == 2)
+                {
+                    diffCounter.Add(sum);
+                }
+
+                diffCounter.Add(userDigits[i] + userDigits[i + 1]);
+            }
+            else
+            {
+                counter++;
+            }
+        }
+
+        if (counter == userDigits.Length / 2)
+        {
+            Console.WriteLine("Yes, value=" + sum);
+        }
+        else
+        {
+            diffCounter.Sort();
+            int difference = diffCounter[diffCounter.Count - 1] - diffCounter[0];
+            Console.WriteLine("No, maxdiff=" + difference);
+        }
+    }
+}
